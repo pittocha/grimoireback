f804309fe0da256bb337a0a51593a9d436f2cf77
Nicolas Martinez <kurulimpah@gmail.com>
2024-01-24 13:52:50 +0100

    route post book fonctionnelle

--- routes/Book.js
+++ routes/Book.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const bookCtrl = require('../controllers/Book')
+const auth = require('../middleware/auth');
+const bookCtrl = require('../controllers/Book');
+const { upload, sharpMiddleware } = require('../middleware/multer-config');
 const router = express.Router();
 
+router.post('/', auth, upload, sharpMiddleware, bookCtrl.createBook);
 router.get('/bestrating', bookCtrl.getTopRatedBooks);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
