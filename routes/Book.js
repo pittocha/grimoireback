5c6c2655ffe1f467eac5d6eca30e28312c66164a
Nicolas Martinez <kurulimpah@gmail.com>
2024-01-26 17:26:00 +0100

    fonction de suppression ajouté

--- routes/Book.js
+++ routes/Book.js
@@ -8,5 +8,6 @@ router.post('/', auth, upload, sharpMiddleware, bookCtrl.createBook);
 router.get('/bestrating', bookCtrl.getTopRatedBooks);
 router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
