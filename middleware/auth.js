b5754eafaa338898d38f2221a917a78f65923e52
Nicolas Martinez <kurulimpah@gmail.com>
2024-03-07 14:02:41 +0100

    application des moidification demmandé

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');//il faudrat remplacer RANDOM_TOKEN_SECRET par un objet generer avec la librairie "crypto"
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
